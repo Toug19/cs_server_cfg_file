5aef33908362b86f32d9326fc5b7df95f626ec1f
Toug19 <59124829+Toug19@users.noreply.github.com>
2024-01-21 18:42:10 +0100

    Add files via upload

---
+++ server_start.bat
@@ -0,0 +1,27 @@
+::start cs2.exe -dedicated -insecure +game_type 2 +game_mode 0 +map de_dust2 +servercfgfile server.cfg
+::start cs2.exe -dedicated -insecure +game_type 2 +game_mode 0 +map de_inferno +servercfgfile server.cfg
+start cs2.exe -dedicated -insecure +map de_inferno +game_alias deathmatch +servercfgfile gamemode_deathmatch2.cfg +sv_setsteamaccount 3C72E91C033F87E93CB2D5B5400E120E
+
+::+sv_setsteamaccount 3C72E91C033F87E93CB2D5B5400E120E
+
+::connect 192.137.3.13:27015; password colomiers31770
+
+:: //Game modes:
+:: Death match: (chacun pour soi) +game_type 1 +game_mode 2
+:: Training: (équipes) +game_type 2 +game_mode 0
+:: Competitive: (5v5) +game_type 0 +game_mode 1
+:: Casual: (10v10)  +game_type 0 +game_mode 0
+:: Wingman: (2v2) +game_type 0 +game_mode 2
+
+:: // Maps: 
+:: cs_italy
+:: cs_office
+:: de_ancient
+:: de_anubis
+:: de_dust2
+:: de_inferno
+:: de_mirage
+:: de_nuke
+:: de_overpass
+:: de_vertigo
+:: lobby_mapveto
